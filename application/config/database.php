7bd4763f97f7dd48d13d26e69508ab4b5e8849f1
Kartik Mandothiya <55804679+kartikya27@users.noreply.github.com>
2022-02-24 15:11:55 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -3,9 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $active_group = 'default';
 $query_builder = TRUE;
+$db['default']['dsn'] = 'pgsql:host=217.21.84.52;port=3306;dbname=u745874161_absc_db';
 
 $db['default'] = array(
-	'dsn'	=> '',
+// 	'dsn'	=> '',
 	'hostname' => '217.21.84.52',
 	'username' => 'u745874161_absc_db_user',
 	'password' => 'Absc_db_password@123',
