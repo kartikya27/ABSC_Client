dfeb77006765f0b0769949352f28d49f8c012cc8
Kartik Mandothiya <55804679+kartikya27@users.noreply.github.com>
2022-02-24 14:47:05 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -6,7 +6,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '217.21.84.52',
+	'hostname' => '217.21.84.52:3306',
 	'username' => 'u745874161_absc_db_user',
 	'password' => 'Absc_db_password@123',
 	'database' => 'u745874161_absc_db',
